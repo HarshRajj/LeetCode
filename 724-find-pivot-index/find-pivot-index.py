eb52b3faea387daab031dfb33a343151c62eeeb6
Harsh Raj <137603566+HarshRajj@users.noreply.github.com>
2024-12-06 23:23:09 +0530

    Time: 14 ms (25.06%) | Memory: 18.7 MB (7.49%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -7,8 +7,10 @@ class Solution:
     
         for i in range(1,n):
             left[i] = left[i-1]+ nums[i-1] 
+
         for i in range(n-2 , -1 , -1) :
             right[i] = right[i+1] + nums [i+1]
+            
         i , j = 0 , 0
         while(i<n and j<n):
             if left[i] == right[i] :
