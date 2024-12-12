3317d436fcd534d44cc12ab785dfe29a29532e9f
Harsh Raj <137603566+HarshRajj@users.noreply.github.com>
2024-12-12 21:33:23 +0530

    Time: 22 ms (37.72%) | Memory: 17.4 MB (20.60%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int)->int :
+        for i in range(k) :
+            gifts.sort()
+            gifts[-1] = int(sqrt(gifts[-1]))
+        return sum(gifts)
+
+
+        
