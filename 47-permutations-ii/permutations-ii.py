1b157829aaad6f0d8322712f7787f3b3b067acec
Harsh Raj <137603566+HarshRajj@users.noreply.github.com>
2025-01-11 22:40:58 +0530

    Time: 4 ms (76.01%) | Memory: 17.9 MB (37.86%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,24 +1,20 @@
 class Solution:
-    def fn(self, res, arr,nums, map): 
-        if len(arr)== len(nums): 
-            res.append(arr[:])
-            return 
-            
-        for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1] and map[i-1]==0 :
-                continue
-            if map[i] == 0 :
-                map[i] = 1 
-                arr.append(nums[i])
-                self.fn(res, arr, nums, map) 
-                arr.pop()
-                map[i] = 0 
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 1 :
-            return [nums]
+        def backtrack(ind):
+            if ind == len(nums):
+                res.append(nums[:])
+                return
+            
+            used = set()
+            for i in range(ind, len(nums)):
+                if nums[i] in used: 
+                    continue
+                used.add(nums[i])
+                nums[ind], nums[i] = nums[i], nums[ind]
+                backtrack(ind + 1)
+                nums[ind], nums[i] = nums[i], nums[ind] 
+
+        res = [] 
         nums.sort() 
-        res = []
-        map = [0]*len(nums) 
-        self.fn(res, [], nums, map) 
+        backtrack(0) 
         return res
-        
