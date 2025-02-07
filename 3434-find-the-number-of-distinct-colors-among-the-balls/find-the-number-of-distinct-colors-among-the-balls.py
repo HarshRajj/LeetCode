fca28b56a2d74db199cc08927a3fa7656e409f6c
Harsh Raj <137603566+HarshRajj@users.noreply.github.com>
2025-02-07 15:02:46 +0530

    Time: 67 ms (78.22%) | Memory: 63.3 MB (31.85%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -1,20 +1,31 @@
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        n=len(queries)
-        ans=[0]*n
-        mp={}
-        color=defaultdict(int)
-        i=0
-        for x, c in queries:
-            if x in mp:
-                c0=mp[x]
-                color[c0]-=1
-                if color[c0]==0:
-                    color.pop(c0)
-            mp[x]=c
-            color[c]+=1
-            ans[i]=len(color)
-            i+=1
-        return ans
-               
+        res = [] # res[i] = distinct # of colors after queries[i]
+        distinct = 0 # current distinct # of colors
+        
+        ball_color = {} # ball : color of the ball
+        color_count = {} # color : count of the color; # of items corresponds to 'distinct'
+
+        for ball, new_color in queries:
+            # Considering the removal of the ball's old color, update 'color_count'
+            if ball in ball_color:
+                old_color = ball_color[ball]
+                color_count[old_color] -= 1
+                if color_count[old_color] == 0:
+                    # A unique color is deleted
+                    del color_count[old_color]
+                    distinct -= 1
+
+            # Update the ball's color and update 'color_count' as appropriate
+            ball_color[ball] = new_color
+            if new_color in color_count:
+                color_count[new_color] += 1
+            else:
+                # A unique color is added
+                color_count[new_color] = 1
+                distinct += 1
+            
+            # Append the distinct # of colors after executing updates
+            res.append(distinct)
+        return res       
         
