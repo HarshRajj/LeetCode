6c109dfad9bc74233ad60743ee8277e0acbcfd2f
Harsh Raj <137603566+HarshRajj@users.noreply.github.com>
2025-02-13 22:01:01 +0530

    Time: 103 ms (100.00%) | Memory: 37.5 MB (18.07%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.py
@@ -1,5 +1,59 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+
+        nums.sort()
+        b = []
+        i = 0
+        j = 0 
+        n = len(nums)
+        m = 0 
+        cnt = 0
+
+        while True :
+            if i < n :
+                x = nums[i]
+                if j<m and b[j] < x :
+                    x = b[j]
+                    j+=1
+                else:
+                    i+=1
+            else:
+                x = b[j]
+                j+=1
+
+            if x>=k :
+                return cnt 
+            if i < n :
+                y = nums[i]
+                if j<m and b[j] < y :
+                    y = b[j]
+                    j+=1
+                else:
+                    i+=1
+            else:
+                y = b[j]
+                j+=1
+
+            b.append( x*2 + y)
+            m+=1 
+            cnt+=1 
+
+        return cnt
+
+
+        
+
+
+
+
+                
+
+
+
+
+
+
+        '''
         min_heap = []
         ans = 0
         for num in nums:
@@ -18,6 +72,9 @@ class Solution:
                 break
 
         return ans
+        '''
+
+
 
         
         
