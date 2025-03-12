0925c2986001225b916f78a88543610661539c75
Harsh Raj <137603566+HarshRajj@users.noreply.github.com>
2025-03-12 15:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (41.77%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,5 +1,8 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
+
+        '''brute---
+
         pos, neg = 0, 0
         for i in nums:
             if i<0 :
@@ -8,6 +11,35 @@ class Solution:
                 pos+=1 
 
         return max(neg, pos)
+
+        '''
+
+        # Binary Search -----
+
+        n = len(nums)
+        start = 0
+        end = n-1 
+        while start <= end :
+            mid = (start+end)//2 
+            if nums[mid] > 0 :
+                end = mid - 1 
+            else:
+                start = mid+1 
+
+        pos = n-start
+
+        start = 0
+        end = n-1 
+        while start<= end :
+            mid = (start+end)//2
+            if nums[mid]<0 :
+                start = mid+1 
+            else:
+                end = mid-1 
+
+        neg = end+1 
+
+        return max(pos, neg)
 
         
 
