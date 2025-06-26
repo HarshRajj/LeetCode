cd05bf9b9cc4506b5de019fd11df85a476867d9b
Harsh Raj <137603566+HarshRajj@users.noreply.github.com>
2025-06-26 23:42:04 +0530

    Time: 4 ms (3.70%) | Memory: 17.8 MB (40.93%) - LeetSync

---
+++ 86-partition-list/partition-list.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+
+        def array_to_linked_list(arr):
+            if not arr:
+                return None
+            head = ListNode(arr[0])
+            current = head
+            for val in arr[1:]:
+                current.next = ListNode(val)
+                current = current.next
+            return head
+
+
+        if not head :
+            return head 
+        if not head.next :
+            return head 
+
+        arr1 = []
+        arr2 = [] 
+
+        while head :
+            if head.val < x:
+                arr1.append(head.val)
+            else:
+                arr2.append(head.val)
+            head = head.next 
+
+        p = array_to_linked_list(arr1)
+        q = array_to_linked_list(arr2)
+
+        head = p
+        while p and p.next :
+            p = p.next 
+        if p!= None:
+            p.next = q 
+        else:
+            head = q
+
+
+        return head
+    
+
+
+
+
+        
