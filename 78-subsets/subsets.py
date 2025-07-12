4b0cb42b0b9c7747a6eb617cfabe0165cc7475b9
Harsh Raj <137603566+HarshRajj@users.noreply.github.com>
2025-07-12 13:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (64.98%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,5 +1,5 @@
 class Solution:
-    def fn(self, ind, nums, ans , ds) :
+    def fn(self, ind, nums, ans , ds) : # backtracking
 
         if ind == len(nums):
             ans.append(ds[:]) 
@@ -10,9 +10,19 @@ class Solution:
         self.fn(ind+1, nums, ans, ds)
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        ds = []
-        self.fn(0, nums, ans, ds) 
-        return ans
+        
+        return self.bit_fn(nums)
 
+        
+    def bit_fn(self, nums): #bit manipulation
+        n = len(nums)
+        ans = []
+        for num in range( 1<<n ) :
+            ls = []
+            for i in range(n):
+                if (num  & (1<<i)) :
+                    ls.append(nums[i])
+            ans.append(ls) 
+
+        return ans
         
