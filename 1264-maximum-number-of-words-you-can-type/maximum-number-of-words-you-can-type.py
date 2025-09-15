1c056ee67069c0294e21be216fdae492c324e615
Harsh <137603566+HarshRajj@users.noreply.github.com>
2025-09-15 08:49:15 +0530

    Time: 3 ms (36.59%) | Memory: 17.8 MB (84.48%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
@@ -1,6 +1,9 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
 
+        return sum(1 for w in text.split() if not any (c in brokenLetters for c in w))
+
+        '''
         res = 0 
         k = 0 
         while k< len(text): 
@@ -10,7 +13,8 @@ class Solution:
             elif text[k] == " " or k == len(text)- 1 :
                 res += 1
             k+= 1
-        return res
+        return res 
+        '''
         
                 
         
