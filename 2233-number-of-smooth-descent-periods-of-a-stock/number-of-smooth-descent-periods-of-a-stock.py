2b3cc80a7d164656e6061dbf5178add8e6a40287
Harsh <137603566+HarshRajj@users.noreply.github.com>
2025-12-15 15:16:13 +0530

    Time: 63 ms (45.90%) | Memory: 29.6 MB (90.30%) - LeetSync

---
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getDescentPeriods(self, prices: List[int]) -> int:
+        ans = 0 
+        cnt = 1
+
+        for i in range (len(prices)):
+            if i == 0 :
+                ans += cnt
+                continue 
+            if prices[i] == prices[i-1]-1:
+                cnt += 1
+
+            else :
+                cnt = 1
+
+            ans += cnt 
+
+        return ans
+
+
+        
