fa0ec669153ef5d0fbd4bfb5e28107fa8a443b28
Harsh <137603566+HarshRajj@users.noreply.github.com>
2026-01-10 22:11:16 +0530

    Time: 191 ms (98.65%) | Memory: 19.6 MB (73.87%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        
+        if len(s1) < len(s2):
+            s1, s2 = s2, s1
+            
+        n, m = len(s1), len(s2)
+        
+        dp = [0] * (m + 1)
+
+        
+        for j in range(m - 1, -1, -1):
+            dp[j] = dp[j + 1] + ord(s2[j])
+
+        
+        for i in range(n - 1, -1, -1):
+            
+            prev = dp[m] 
+            
+            # Update the last element (base case: if s2 is empty, delete s1[i])
+            dp[m] += ord(s1[i])
+
+            for j in range(m - 1, -1, -1):
+                temp = dp[j]
+                
+                if s1[i] == s2[j]:
+                    
+                    dp[j] = prev
+                else:
+                    
+                    dp[j] = min(
+                        ord(s1[i]) + dp[j], 
+                        ord(s2[j]) + dp[j + 1]
+                    )
+                
+                prev = temp
+
+        return dp[0]
